74d79d280443e7f2fad59c8cc33fc97ef020b65b
minkinghub <kp141@naver.com>
2023-07-18 15:51:13 +0900

    asda

--- app/src/main/java/com/example/myapplication/OtherActivity.java
+++ app/src/main/java/com/example/myapplication/OtherActivity.java
@@ -1,32 +1,26 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
-
+public class OtherActivity extends AppCompatActivity {
     private TextView textView;
     private Handler handler;
     private Runnable runnable;
     private long startTime;
-    private Button button;
-    private MediaPlayer mediaPlayer;
-    private int[] audioResources = {R.raw.hello, R.raw.hello2, R.raw.hello3, R.raw.hello4};
-    private int currentAudioIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_other);
 
         textView = findViewById(R.id.text_view);
         handler = new Handler();
@@ -47,11 +41,6 @@ public class MainActivity extends AppCompatActivity {
                 // 텍스트뷰에 현재 시간 표시
                 textView.setText(currentTime);
 
-                // 특정 시간에 오디오 파일 재생
-                if (seconds == 5 || seconds == 15 || seconds == 25 || seconds == 35) {
-                    playAudio();
-                }
-
                 // 다음 호출을 1초 후에 예약
                 handler.postDelayed(this, 1000);
             }
@@ -59,16 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
         // 최초 호출을 1초 후에 예약
         handler.postDelayed(runnable, 1000);
-
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 버튼 클릭 시 다른 페이지로 이동
-                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     @Override
@@ -76,22 +55,5 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         // 액티비티가 종료되면 예약된 호출 취소
         handler.removeCallbacks(runnable);
-
-        // MediaPlayer 정리
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }
     }
-
-    private void playAudio() {
-        // 현재 인덱스에 해당하는 오디오 파일 재생
-        if (currentAudioIndex < audioResources.length) {
-            Uri audioUri = Uri.parse("android.resource://" + getPackageName() + "/" + audioResources[currentAudioIndex]);
-            mediaPlayer = MediaPlayer.create(this, audioUri);
-            mediaPlayer.start();
-            currentAudioIndex++;
-        }
-    }
-
 }
