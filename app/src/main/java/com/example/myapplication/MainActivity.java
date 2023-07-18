f3dd76933c01677ffd1ccf1cbd4ba383adef993e
minkinghub <kp141@naver.com>
2023-07-18 12:01:55 +0900

    asd

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,23 +1,51 @@
 package com.example.myapplication;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Handler handler;
+    private Runnable runnable;
+    private String[] textArray;
+    private int currentIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        handler = new Handler();
+        textArray = new String[]{"첫 번째 텍스트", "두 번째 텍스트", "세 번째 텍스트"};
+        currentIndex = 0;
+
+        // 5초마다 텍스트 업데이트를 위한 Runnable 생성
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                // 출력할 텍스트 선택
+                String text = textArray[currentIndex];
+
+                // 다음 인덱스로 이동
+                currentIndex = (currentIndex + 1) % textArray.length;
+
+                // 다음 호출을 5초 후에 예약
+                handler.postDelayed(this, 10000);
+            }
+        };
+
+        // 최초 호출을 5초 후에 예약
+        handler.postDelayed(runnable, 10000);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 액티비티가 종료되면 예약된 호출 취소
+        handler.removeCallbacks(runnable);
+    }
 }
+
