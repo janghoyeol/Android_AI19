784616185bfe8a9760bb16e7001cfb22aa1824ed
minkinghub <kp141@naver.com>
2023-07-18 10:12:43 +0900

    asdf

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,35 +12,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_PHONE_PERMISSION = 1;
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonCall = findViewById(R.id.buttonCall);
-        buttonCall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 통화 기능을 사용하기 위해 필요한 권한을 확인하고 요청합니다.
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_PHONE_PERMISSION);
-                } else if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO_PERMISSION);
-                } else {
-                    // 통화 화면으로 이동합니다.
-                    startCall();
-                }
-            }
-        });
     }
 
-    private void startCall() {
-        Intent intent = new Intent(MainActivity.this, CallActivity.class);
-        startActivity(intent);
-    }
-
-    // ...
 }
